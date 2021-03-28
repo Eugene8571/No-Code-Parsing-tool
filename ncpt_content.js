255fa2b884254c2d357cf46ab2bef17d81dfa0d0
Eugene8571 <40310109+Eugene8571@users.noreply.github.com>
2021-03-28 20:26:07 +0200

    splitting large tool.activate

--- ncpt_content.js
+++ ncpt_content.js
@@ -393,15 +393,10 @@ const tool = {
 		}
 	},
 	
-
-	activate: function() {
-
-		function lockPage()
-		{
-		  lockElements(document.getElementsByTagName("a"));
-		  lockElements(document.getElementsByTagName("input"));
-		  lockElements(document.getElementsByTagName("button"));
-		};
+	lockPage: function() {
+		lockElements(document.getElementsByTagName("a"));
+		lockElements(document.getElementsByTagName("input"));
+		lockElements(document.getElementsByTagName("button"));
 
 		function lockElements(el)
 		{
@@ -410,11 +405,11 @@ const tool = {
 		    el[i].style.pointerEvents="none";
 		  }
 		};
-				
-		lockPage();
 
-		if (!tool.helpWindow) tool.updateCSS();
 
+	},
+
+	helpWindowSpawn: function() {
 		let div = document.createElement('div');
 		div.setAttribute("id", "tool_wnd");
 		document.body.appendChild(div);
@@ -483,21 +478,19 @@ const tool = {
 		  </div>
 
 		`;
+	},
 
-		//Make the DIV element draggagle:
-		dragElement(document.getElementById("tool_wnd"));
-
-		function dragElement(elmnt) {
-		  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-		  if (document.getElementById(elmnt.id + "_header")) {
+	makeDraggable: function(elmnt) {
+		var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+		if (document.getElementById(elmnt.id + "_header")) {
 		  	/* if present, the header is where you move the DIV from:*/
 		    document.getElementById(elmnt.id + "_header").onmousedown = dragMouseDown;
-		  } else {
+		} else {
 		    /* otherwise, move the DIV from anywhere inside the DIV:*/
 		    elmnt.onmousedown = dragMouseDown;
-		  }
+		}
 
-		  function dragMouseDown(e) {
+		function dragMouseDown(e) {
 		    e = e || window.event;
 		    e.preventDefault();
 		    // get the mouse cursor position at startup:
@@ -506,9 +499,9 @@ const tool = {
 		    document.onmouseup = closeDragElement;
 		    // call a function whenever the cursor moves:
 		    document.onmousemove = elementDrag;
-		  }
+		}
 
-		  function elementDrag(e) {
+		function elementDrag(e) {
 		    e = e || window.event;
 		    e.preventDefault();
 		    // calculate the new cursor position:
@@ -519,15 +512,25 @@ const tool = {
 		    // set the element's new position:
 		    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
 		    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-		  }
+		}
 
-		  function closeDragElement() {
+		function closeDragElement() {
 		    /* stop moving when mouse button is released:*/
 		    document.onmouseup = null;
 		    document.onmousemove = null;
-		  }
 		}
+	},
 
+	activate: function() {
+
+		tool.lockPage();
+
+		if (!tool.helpWindow) tool.updateCSS();
+
+		tool.helpWindowSpawn()
+
+		let div = document.getElementById("tool_wnd");
+		tool.makeDraggable(div);
 
 		// Events
 
