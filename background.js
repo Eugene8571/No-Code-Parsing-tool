fdde751013a0e27a4793eab71e691e792c702f2e
Eugene8571 <40310109+Eugene8571@users.noreply.github.com>
2021-03-30 07:50:32 +0200

    automate start

--- background.js
+++ background.js
@@ -54,13 +54,20 @@ chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.getSelected(null, function(tab) {
 		chrome.tabs.sendMessage(tab.id, { 'action': 'toggle' }, function(response) {
 			if (chrome.runtime.lastError) {
-				// lastError needs to be checked, otherwise Chrome may throw an error
+				// lastError needs to be checked
 			}
 
 			if (!response) {
 				chrome.tabs.executeScript(tab.id, {
 					code: "location.reload();"
 				});
+				setTimeout(function () { 
+					chrome.tabs.sendMessage(tab.id, { 'action': 'toggle' }, function(response) {
+						if (chrome.runtime.lastError) {
+							// lastError needs to be checked
+						}
+					});
+				}, 1500);
 			}
 		});
 	});
