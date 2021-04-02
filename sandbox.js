772bc3df3623a8f49daa36f8ac7e044dc3f39e17
Eugene8571 <40310109+Eugene8571@users.noreply.github.com>
2021-04-03 00:38:29 +0200

    Create sandbox.js

---
+++ sandbox.js
@@ -0,0 +1,24 @@
+
+let A = [];
+A.push('...items', '1',23, {value:'qwe', name:"Ro"})
+console.log(A.length, A);
+console.log({A});
+
+let D = {};
+D.a = 'a'
+D.b = 1
+
+delete D.a
+
+console.log({D})
+console.log(D.a)
+
+line = {}
+line.area = 'area'
+line.row = 'row'
+
+line.columns = {}
+
+line.columns.id1 = 'val1'
+
+console.log(line)
