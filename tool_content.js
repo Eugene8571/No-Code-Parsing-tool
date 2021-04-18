d1a5e61e1a7877b99811de700abd110b46802059
Eugene8571 <40310109+Eugene8571@users.noreply.github.com>
2021-04-18 19:21:58 +0200

    copy to clipboard

--- tool_content.js
+++ tool_content.js
@@ -312,11 +312,39 @@ const tool = {
             btns[i].addEventListener('mousedown', tool.activateSelectMod, true)
         }
 
-        div.querySelector('#tool_next').addEventListener('mousedown', tool.activateSelectMod, true)
+        div.querySelector('#tool_next').addEventListener(
+            'mousedown', tool.activateSelectMod, true)
 
-        div.querySelector('#tool_display').addEventListener('mousedown', tool.toggleDisplay, true)
+        div.querySelector('#tool_display').addEventListener(
+            'mousedown', tool.toggleDisplay, true)
+        div.querySelector('#tool_selected_elm_copy').addEventListener(
+            'mousedown', tool.copySelectedElemPath, true)
 
 
+
+    },
+
+    copySelectedElemPath: function(e) {
+        path = document.querySelector('#tool_selected_elm').innerText
+        if (window.clipboardData && window.clipboardData.setData) {
+            // IE specific code path to prevent textarea being shown while dialog is visible.
+            return clipboardData.setData("Text", path); 
+
+        } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+            var textarea = document.createElement("textarea");
+            textarea.textContent = path;
+            textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+            document.body.appendChild(textarea);
+            textarea.select();
+            try {
+                return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+            } catch (ex) {
+                console.warn("Copy to clipboard failed.", ex);
+                return false;
+            } finally {
+                document.body.removeChild(textarea);
+            }
+        }
     },
 
     toggleDisplay: function() {
